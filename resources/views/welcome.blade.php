5d8f2bdaad5c01a7620a01d8ef78477be6be4c64
elegarmi <elegarmi@gmail.com>
2022-02-24 21:32:26 +0100

    Added welcome media queries

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content') 
     <div id="welcome-bg">
         <div id="welcome-container">
-            <div id="welcome-text">
+            <div id="welcome-text" class="col col-lg-6 col-md-12">
                 {{-- <h1 class="section-title"><span class="section-title-line">Candidaturas</span></h1> --}}
                 <h1 class="section-title">Candidaturas</h1>
                 <p>Gestión de escuelas, promociones y candidaturas</p>
@@ -12,7 +12,7 @@
                     <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                 </div>
             </div>
-            <div id="welcome-img">
+            <div id="welcome-img" class="col col-lg-6 col-md-12">
                 <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
