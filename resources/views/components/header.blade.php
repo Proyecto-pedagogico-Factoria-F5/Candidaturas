749162f34345cd01bd145f1238e006f9a55916b0
elegarmi <elegarmi@gmail.com>
2022-03-02 11:26:29 +0100

    Restored basic structure and styles

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,18 +3,23 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name') }}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto">
-
+            @auth()
+            <ul class="navbar-nav mr-auto">
+                <!--Nav Bar Hooks - Do not delete!!-->
+                <li class="nav-item">
+                    <a href="{{ url('/schools') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Escuelas</a> 
+                </li>
             </ul>
-
+            @endauth()
+            
             <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
                     @if (Route::has('login'))
@@ -22,21 +27,20 @@
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
                         </li>
                     @endif
-
+                    
                     @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Dar de alta') }}</a>
                         </li>
                     @endif
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('/home') }}">Escuelas</a>
-                            <a class="dropdown-item dropdown-last-item" href="{{ route('logout') }}"
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Cerrar sesión') }}
