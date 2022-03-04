42bc010456dec645fb2005c53694334fe35999ef
elegarmi <elegarmi@gmail.com>
2022-03-04 12:36:10 +0100

    MVP styles improved

--- resources/views/livewire/components/card-promo.blade.php
+++ resources/views/livewire/components/card-promo.blade.php
@@ -1,19 +1,15 @@
-<div >
-  @foreach ($promos as $promo)
-  <div class="cards-container">
-    <div id="card">
-      <img class="card-img-top" src="..." alt="Card image cap">
-      <div class="card-body">
-        <h2 class="card-title">{{ $promo->ubicación }}</h2>
-        <p class="card-text">{{ $promo->fecha_de_inicio }}</p>
-        <p class="card-text">{{ $promo->duración }}</p>
-      </div>
-      <div class="card-body">
-        <h1 href="#" class="card-link">{{ $promo->nombre_promo }}</h1>
-      </div>
+@foreach ($promos as $promo)
+<div class="cards-container">
+  <div id="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">{{ $promo->ubicación }}</h2>
+      <p class="card-text">{{ $promo->fecha_de_inicio }}</p>
+      <p class="card-text">{{ $promo->duración }}</p>
+    </div>
+    <div class="card-body">
+      <h1 href="#" class="card-link">{{ $promo->nombre_promo }}</h1>
     </div>
   </div>
-  @endforeach  
 </div>
-
-{{-- style="width: 18rem;" --}}
+@endforeach  
--- resources/views/livewire/components/card-school.blade.php
+++ resources/views/livewire/components/card-school.blade.php
@@ -1,9 +1,8 @@
-
-  @foreach ($schools as $school)
-    <div class="cards-container">
-      <div id="card">
-        <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
-        <h1 class="card-title">{{ $school->provincia }}</h1>
-      </div>
+@foreach ($schools as $school)
+  <div class="cards-container">
+    <div id="card">
+      <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
+      <h1 class="card-title">{{ $school->provincia }}</h1>
     </div>
-  @endforeach
+  </div>
+@endforeach
