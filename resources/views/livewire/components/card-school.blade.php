5969764b9b2a95cb9beacb55e3bd9c59746cbb10
elegarmi <elegarmi@gmail.com>
2022-03-04 18:23:23 +0100

    Restored flaticon, responsive cards, profile route and nav link

--- resources/views/livewire/components/card-school.blade.php
+++ resources/views/livewire/components/card-school.blade.php
@@ -1,6 +1,8 @@
 @foreach ($schools as $school)
-  <div id="card">
-    <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
-    <h1 class="card-title">{{ $school->provincia }}</h1>
+<div class="card col-lg-4 col-md-6 col-xs-12">
+  <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
+  <div class="card-body">
+    <h5 class="card-title">{{ $school->provincia }}</h5>
   </div>
+</div>
 @endforeach
--- resources/views/schools-view.blade.php
+++ resources/views/schools-view.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div class="container-fluid">
-	<div class="row justify-content-center">
-		<div class="col-md-12">
-			<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
-			<div class="cards-container">
-				@livewire('components.card-school')	
+	<div>
+		<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
+		<div class="container cards-container">
+			<div class="row justify-content-around gap-5">
+				@livewire('components.card-school')
 			</div>
 		</div>
 	</div>
