26353ef75e7e67f2a03073ebd2f5915e1561fec1
EmmaFC <emmafecor@hotmail.com>
2022-03-04 02:09:36 +0100

    adding styles

--- resources/views/schools-view.blade.php
+++ resources/views/schools-view.blade.php
@@ -6,13 +6,13 @@
 	<div class="row justify-content-center">
 		<div class="container">
 			<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
-			<div class="rows-fluid">
+		{{-- 	<div class="rows-fluid">
 				<div class="container_01">
-				<div class="card-columns-fluid">
+		{{-- 		<div class="card-columns-fluid"> --}}
 				@livewire('components.card-school')					
-			</div>
-	{{-- 	</div> --}}
-		</div>
+		{{-- 	</div>
+	{{-- 	</div> 
+		</div> --}}
 		</div>
 	</div>
 </div>
