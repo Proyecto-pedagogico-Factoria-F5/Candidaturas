b4bcf357251f93f894327e1a6c797878884a77a9
elegarmi <elegarmi@gmail.com>
2022-03-04 11:24:29 +0100

    Images upload working

--- resources/views/livewire/components/card-school.blade.php
+++ resources/views/livewire/components/card-school.blade.php
@@ -2,8 +2,8 @@
   @foreach ($schools as $school)
     <div class="cards-container">
       <div id="card">
-        <img class="card-img-top" src={{ $school->imagen }} alt="Card" />
+        <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="Card" />
         <h1 class="card-title">{{ $school->provincia }}</h1>
       </div>
     </div>
-  @endforeach  
+  @endforeach
