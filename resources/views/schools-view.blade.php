d8318b72948450edc50f003cd779fcdc566f9c3e
elegarmi <elegarmi@gmail.com>
2022-03-04 11:56:45 +0100

    School view indentation improved

--- resources/views/schools-view.blade.php
+++ resources/views/schools-view.blade.php
@@ -7,9 +7,7 @@
 		<div class="container">
 			<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
 			<div class="row container_01">
-		
-						@livewire('components.card-school')					
-
+				@livewire('components.card-school')					
 			</div> 
 		
 		</div>
