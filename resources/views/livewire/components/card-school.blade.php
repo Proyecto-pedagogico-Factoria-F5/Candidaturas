fd96be3db2632c2421b372983e703be40cc6180a
elegarmi <elegarmi@gmail.com>
2022-03-04 11:34:21 +0100

    Images alt attempt

--- resources/views/livewire/components/card-school.blade.php
+++ resources/views/livewire/components/card-school.blade.php
@@ -2,7 +2,7 @@
   @foreach ($schools as $school)
     <div class="cards-container">
       <div id="card">
-        <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="Card" />
+        <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
         <h1 class="card-title">{{ $school->provincia }}</h1>
       </div>
     </div>
