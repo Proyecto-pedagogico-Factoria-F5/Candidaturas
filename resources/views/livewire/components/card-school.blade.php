6bde07b02ec18a4db4183b1569ce3d7403b19e05
elegarmi <elegarmi@gmail.com>
2022-03-04 13:28:05 +0100

    Card styles and schools view improved

--- resources/views/livewire/components/card-school.blade.php
+++ resources/views/livewire/components/card-school.blade.php
@@ -1,8 +1,6 @@
 @foreach ($schools as $school)
-  <div class="cards-container">
-    <div id="card">
-      <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
-      <h1 class="card-title">{{ $school->provincia }}</h1>
-    </div>
+  <div id="card">
+    <img class="card-img-top" src="{{ asset('storage').'/'.$school->imagen }}" alt="{{ $school->name }}" />
+    <h1 class="card-title">{{ $school->provincia }}</h1>
   </div>
 @endforeach
--- resources/views/schools-view.blade.php
+++ resources/views/schools-view.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 
 <div class="container-fluid">
-	<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
-	<div class="col">
-		@livewire('components.card-school')					
-	</div> 
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<h1 class="display-6 mb-5 section-title"><span class="section-title-line">Escuelas</span></h1>
+			<div class="cards-container">
+				@livewire('components.card-school')	
+			</div>
+		</div>
+	</div>
 </div>
 
 @endsection
