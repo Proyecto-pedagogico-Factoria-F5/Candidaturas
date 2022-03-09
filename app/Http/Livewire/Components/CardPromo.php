c87d27794648f448270aeb851a346f9b6f17ade7
EmmaFC <emmafecor@hotmail.com>
2022-03-09 14:35:05 +0100

    adding test list

--- app/Http/Livewire/Components/CardPromo.php
+++ app/Http/Livewire/Components/CardPromo.php
@@ -10,7 +10,6 @@ class CardPromo extends Component
     public function render()
     {
         $promos = Promo::all();
-        $promo_school = School::where('id', $row->escuela_id);
-        return view('livewire.components.card-promo', ['promos'=> $promos, 'promo_school' => $promo_school]);
+        return view('livewire.components.card-promo', ['promos'=> $promos]);
     }
 }
