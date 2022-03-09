185a9ec9fc9b5f0d9e66ab558c9c71fb49015dc2
Sara <sara.gonzalezsa@gmail.com>
2022-03-09 10:00:59 +0100

    solved migrations token

--- database/migrations/2022_03_09_080641_create_tokens_table.php
+++ database/migrations/2022_03_09_080641_create_tokens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
-            $table->token('token');
+            $table->string('token');
             $table->timestamps();
         });
     }
