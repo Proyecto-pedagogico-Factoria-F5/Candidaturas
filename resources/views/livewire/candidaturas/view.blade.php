8ab643dda8e1ca822ffdaeec14654a27948ead63
EmmaFC <emmafecor@hotmail.com>
2022-03-16 23:57:54 +0100

    fixing orange div

--- resources/views/livewire/candidaturas/view.blade.php
+++ resources/views/livewire/candidaturas/view.blade.php
@@ -22,62 +22,62 @@
 						<i class="fa fa-plus"></i> Añadir candidatura
 						</div>
 						<a href="{{url('/send/email')}}" class="btn btn-sm btn-info"> 
-						<i class="fa fa-plus"></i> Enviar Email</button>
-						</div>
+						<i class="fa fa-plus"></i> Enviar Email</a>
+					</div>
 				</div>
 				
 				<div class="card-body">
-						@include('livewire.candidaturas.create')
-						@include('livewire.candidaturas.update')
-				<div class="table-responsive">
-					<table class="table table-bordered table-sm">
-						<thead class="thead">
-							<tr> 
-								<td>#</td> 
-								<th>Nombre</th>
-								<th>Apellidos</th>
-								<th>Fecha de nacimiento</th>
-								<th>Nacionalidad</th>
-								<th>Email</th>
-								<th>Teléfono</th>
-								<th>Cuenta de usuario</th>
-								<th>Puntos</th>
-								<th>Descripción</th>
-								<th>Fecha de registro</th>
-								<th>Promo id</th>
-								<td>Acciones</td>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach($candidaturas as $row)
-							<tr>
-								<td>{{ $loop->iteration }}</td> 
-								<td>{{ $row->nombre }}</td>
-								<td>{{ $row->apellidos }}</td>
-								<td>{{ $row->fecha_de_nacimiento }}</td>
-								<td>{{ $row->nacionalidad }}</td>
-								<td>{{ $row->email }}</td>
-								<td>{{ $row->teléfono }}</td>
-								<td>{{ $row->cuenta_usuario }}</td>
-								<td>{{ $row->puntos }}</td>
-								<td>{{ $row->descripción }}</td>
-								<td>{{ $row->fecha_de_registro }}</td>
-								<td>{{ $row->promo_id }}</td>
-								<td width="90">
-								<div class="btn-group">
-									<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Acciones
-									</button>
-									<div class="dropdown-menu dropdown-menu-right">
-									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Editar </a>							 
-									<a class="dropdown-item" onclick="confirm('¿Confirmas que quieres borrar la candidatura con id {{$row->id}}? \n¡Esta acción no se puede deshacer!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Borrar </a>   
+					@include('livewire.candidaturas.create')
+					@include('livewire.candidaturas.update')
+					<div class="table-responsive">
+						<table class="table table-bordered table-sm">
+							<thead class="thead">
+								<tr> 
+									<td>#</td> 
+									<th>Nombre</th>
+									<th>Apellidos</th>
+									<th>Fecha de nacimiento</th>
+									<th>Nacionalidad</th>
+									<th>Email</th>
+									<th>Teléfono</th>
+									<th>Cuenta de usuario</th>
+									<th>Puntos</th>
+									<th>Descripción</th>
+									<th>Fecha de registro</th>
+									<th>Promo id</th>
+									<td>Acciones</td>
+								</tr>
+							</thead>
+							<tbody>
+								@foreach($candidaturas as $row)
+								<tr>
+									<td>{{ $loop->iteration }}</td> 
+									<td>{{ $row->nombre }}</td>
+									<td>{{ $row->apellidos }}</td>
+									<td>{{ $row->fecha_de_nacimiento }}</td>
+									<td>{{ $row->nacionalidad }}</td>
+									<td>{{ $row->email }}</td>
+									<td>{{ $row->teléfono }}</td>
+									<td>{{ $row->cuenta_usuario }}</td>
+									<td>{{ $row->puntos }}</td>
+									<td>{{ $row->descripción }}</td>
+									<td>{{ $row->fecha_de_registro }}</td>
+									<td>{{ $row->promo_id }}</td>
+									<td width="90">
+									<div class="btn-group">
+										<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										Acciones
+										</button>
+										<div class="dropdown-menu dropdown-menu-right">
+										<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Editar </a>							 
+										<a class="dropdown-item" onclick="confirm('¿Confirmas que quieres borrar la candidatura con id {{$row->id}}? \n¡Esta acción no se puede deshacer!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Borrar </a>   
+										</div>
 									</div>
-								</div>
-								</td>
-							@endforeach
-						</tbody>
-					</table>						
-					{{ $candidaturas->links() }}
+									</td>
+								@endforeach
+							</tbody>
+						</table>						
+						{{ $candidaturas->links() }}
 					</div>
 				</div>
 			</div>
