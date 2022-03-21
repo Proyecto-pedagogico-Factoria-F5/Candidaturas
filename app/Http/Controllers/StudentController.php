d59de00a9b391707f9907b8ddea89dc143bb8a62
EmmaFC <emmafecor@hotmail.com>
2022-03-21 10:23:56 +0100

    adding email mvp

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,40 +5,57 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\MySendMail;
+use App\Models\Admin;
 use App\Models\Candidatura;
 use App\Models\Promo;
 use App\Models\School;
 
 class StudentController extends Controller
 {
-    public function mail($current_promo_id, $id)
-	{
-        //Comprobar si funciona el filtrado de datos	
-		$student_detail = [
-			'nombre' => Candidatura::find($id)->nombre,
-			'apellidos' => Candidatura::find($id)->apellidos,
-			'email' => Candidatura::find($id)->email,
+    public function mail(/* $student_id */)
+	{	
+		//Falta filtrar el $student_id de los seleccionados y pasarlo como parámetro en mail()
+		$student_id =3;
+
+		$promo_id = Candidatura::find($student_id)->promo_id;
+		$school_id = Promo::find($promo_id)->escuela_id;
+		
+		// Datos del admin de la promoción (comprobar código comentado)
+				/* foreach ($promo->admins as $table_admins) {
+				   $table_admins = $table_admins->pivot->admin_id; 
+				   }  */
+		$admin_detail = [
+			'nombre_admin' => 'Carla', /*  Admin::find($table_admins)->name, */
+			'email_admin' => 'factoriaf5@gmail.com' /*  Admin::find($table_admins)->email */
 		];
 		
-		$current_promo = Promo::find($id);
-		$current_school_id = Promo::where('id', $current_promo_id)->escuela_id;
-		
-		foreach ($promo->admins as $table_admins) { 
-			$table_admins = $table_admins->pivot->admin_id;
-        } 
-		
-		$admin_name = Admin::find($table_admins)->name;
-		$admin_email = Admin::find($table_admins)->email;
-
+		// Datos de la promoción
 		$promo_detail = [
-			'nombre_escuela' => School::where('id', $current_school_id)->nombre_escuela,
-			'nombre_promo' => Promo::where('id', $current_promo_id)->nombre_promo,
-			'email' => Admin::find($current_promo_id)->email,
+			'nombre_escuela' => School::find($school_id)->nombre_escuela,
+			'nombre_promo' => Promo::find($promo_id)->nombre_promo
 		];
 
-		$mail = new MySendMail($student_detail);
+     	// Datos del candidato
+		$student_detail = [
+			'nombre' => Candidatura::find($student_id)->nombre,
+			'apellidos' => Candidatura::find($student_id)->apellidos,
+			'email' => Candidatura::find($student_id)->email,
+		];
+
+		/* Esta parte la tiene que poder introducir Alí */
+		$email_detail = [
+			'titulo' => 'Título del email',
+			'contenido' => 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum ',
+			'asunto' => 'Este es el asunto del email',
+		];
+
+		// Envío del email como componente
+		$mail = new MySendMail($email_detail, $promo_detail, $admin_detail, $student_detail);
 		Mail::to ($student_detail['email'])->send($mail);
-		return 'Email has been sent';
+
+		// Redirección (corregir ruta cuando se compruebe que funciona)
+		return 'Email enviado con éxito!! Revisa la bandeja de Mailtrap';
+		// return redirect(url('/candidaturas'));
 		
 		}
 }
--- app/Mail/MySendMail.php
+++ app/Mail/MySendMail.php
@@ -10,14 +10,20 @@ use Illuminate\Queue\SerializesModels;
 class MySendMail extends Mailable
 {
     use Queueable, SerializesModels;
+    public $email_detail;
+    public $promo_detail;
+    public $admin_detail;
     public $student_detail;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($student_detail)
+    public function __construct($email_detail, $promo_detail, $admin_detail, $student_detail)
     {
+        $this->email_detail = $email_detail;
+        $this->promo_detail = $promo_detail;
+        $this->admin_detail = $admin_detail;
         $this->student_detail = $student_detail;
     }
 
@@ -28,6 +34,6 @@ class MySendMail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.mySendMail');
+        return $this->subject($this->email_detail['asunto'])->view('emails.mySendMail');
     }
 }
