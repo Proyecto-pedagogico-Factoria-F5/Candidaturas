f74ad91bd1662a2cbaa58d646338005ac0639c56
imoreno73 <92292636+imoreno73@users.noreply.github.com>
2022-03-23 12:01:44 +0100

    undo commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\MySendMail;
-use App\Models\Admin;
 use App\Models\Candidatura;
-use App\Models\Promo;
-use App\Models\School;
 
 class StudentController extends Controller
 {
@@ -27,22 +24,9 @@ class StudentController extends Controller
         'apellidos' =>  Candidatura::find($id)->apellidos,
 		'email' => Candidatura::find($id)->email,
 		];
-
-		/* Esta parte la tiene que poder introducir Alí */
-		$email_detail = [
-			'titulo' => 'Título del email',
-			'contenido' => 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum ',
-			'asunto' => 'Este es el asunto del email',
-		];
-
-		// Envío del email como componente
-		$mail = new MySendMail($email_detail, $promo_detail, $admin_detail, $student_detail);
-		Mail::to ($student_detail['email'])->send($mail);
-
-		// Redirección (corregir ruta cuando se compruebe que funciona)
-		return 'Email enviado con éxito!! Revisa la bandeja de Mailtrap';
-		// return redirect(url('/candidaturas'));
-		
-		}
+	   Mail::to ($student_detail['email'])->send(new MySendMail($student_detail));
+	   
+	   return 'Email has been sent';
+	}
 }
 
