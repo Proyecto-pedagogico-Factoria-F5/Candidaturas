5ba1a3f900f25e94383ceacb35395c287b428a0c
rubenbch76 <rubenbch@gmail.com>
2022-03-23 10:57:29 +0100

    id promo loaded from DB. Token loaded from DB. All works well

--- resources/views/livewire/candidaturas/view.blade.php
+++ resources/views/livewire/candidaturas/view.blade.php
@@ -19,14 +19,14 @@
 						<div>
 							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Buscar candidaturas">
 						</div>
-						{{-- <div class="btn btn-sm btn-info" data-toggle="modal" data-target="#createDataModal">
+						<div class="btn btn-sm btn-info" data-toggle="modal" data-target="#createDataModal">
 							<i class="fa fa-plus"></i> Añadir candidatura
-						</div> --}}
+						</div>
 					</div>
 				</div>
 				<div class="card-body">
-					{{-- @include('livewire.candidaturas.create')
-					@include('livewire.candidaturas.update') --}}
+					@include('livewire.candidaturas.create')
+					@include('livewire.candidaturas.update')
 					<div class="table-responsive" wire:ignore>
 						<table class="table table-bordered table-sm">
 							<thead class="thead">
@@ -46,8 +46,7 @@
 									<th>Acciones</th>
 								</tr>
 							</thead>
-							<tbody id="tbody"></tbody>
-							{{-- <tbody>
+							<tbody>
 								@foreach($candidaturas as $row)
 								<tr>
 									<td>{{ $loop->iteration }}</td> 
@@ -74,7 +73,7 @@
 									</div>
 									</td>
 								@endforeach
-							</tbody> --}}
+							</tbody>
 						</table>						
 						{{ $candidaturas->links() }}
 					</div>
@@ -83,174 +82,3 @@
 		</div>
 	</div>
 </div>
-
-
-
-<script>
-
-// FREECODECAMP ************************************************************************************************
-
-const urlFreeCodeCamp = 'https://api.freecodecamp.org/api/users/get-public-profile?username='
-
-var userArrayPoints = [];
-
-async function getPoints(id){
-
-    var myHeaders = new Headers();
-    myHeaders.append("Cookie", "_csrf=XgWIagMZB3DIJKmiNWw2_yBX; csrf_token=CJzGy40i-Yn-nkwiiJx3JjVtrhFBioB91v_8; connect.sid=s%3AOYbepHrYbtwH5AdlKH_z1IiVBjGdhbMo.YuGP9bE7xSFH9UMpfUx5eREwD2PWGitHqJH315CrPqo");
-
-    var requestOptions = {
-		method: 'GET',
-		headers: myHeaders,
-		redirect: 'follow',
-    };
-
-	try{
-		let response = await fetch(urlFreeCodeCamp+id, requestOptions);
-		let result = await response.json();
-
-		let userPoints = await result.entities.user[id].points;
-
-		return userPoints;
-	}catch(err){
-		console.error(err);
-	}	
-}
-
-async function getArrayPoints(id){
-
-	userArrayPoints.push({id:id, points:await getPoints(id)});
-}
-
-
-
-
-// TYPEFORM ****************************************************************************************************
-
-const urlTypeFormPromos = 'https://api.typeform.com/forms';
-const tokenTypeForm = 'tfp_GnZuL3sjSVmRL8NEUeZxyHVbqgLJbkFoMp6pbUavcFSq_3sqakqAhZEr4b';  //Este dato habrá que sacarlo de la BD -> tabla token
-const idPromo = 'pY7hQ52y'  //Este dato habrá que sacarlo de la BD -> tabla promos
-
-const NAME = 0;
-const SURNAME = 1;
-const MAIL = 2;
-const FREECODE = 3;
-const DESCRIPTION = 4;
-
-var arrayFreeCodeIds = [];
-
-async function getCandidatures(idPromo){
-
-    const urlTypeFormCandidatures = 'https://api.typeform.com/forms/' + idPromo + '/responses';
-
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + tokenTypeForm + "S");
-
-    var requestOptions = {
-		method: 'GET',
-		headers: myHeaders,
-		redirect: 'follow'
-    };
-
-	try{
-		let response = await fetch(urlTypeFormCandidatures, requestOptions);
-		let result = await response.json();
-		let typeFormCandidatures = await result.items;
-		return typeFormCandidatures;
-
-	}catch(err){
-		console.error(err);
-	} 
-}
-
-async function getFreeCodeIds(){
-
-	let typeFormPromos = await getCandidatures(idPromo);
-
-	typeFormPromos.forEach(user => {   
-
-		user.answers.forEach(element => {  
-			
-			if(user.answers.indexOf(element) == FREECODE) {
-				arrayFreeCodeIds.push(element.text);
-			} 				
-		}); 	
-	});
-}
-
-async function showCandidatures(){
-
-    let typeFormPromos = await getCandidatures(idPromo);
-
-	var row;
-	var rowsCandidates = [];
-	var idFreeCode;
-	
-	typeFormPromos.forEach(user => {   
-	
-		row = '<tr><td></td>';
-		
-		user.answers.forEach(element => {  
-					
-			if(user.answers.indexOf(element) == NAME) {
-				row+= '<td>'+ element.text + '</td>';
-			}    
-			if(user.answers.indexOf(element) == SURNAME) {
-				row+= '<td>'+ element.text + '</td>';
-			}    
-			if(user.answers.indexOf(element) == MAIL) {  
-				row+= '<td>Fecha</td>';  
-				row+= '<td>Nacionalidad</td>';    
-				row+= '<td>'+ element.email + '</td>';  
-			}
-			if(user.answers.indexOf(element) == FREECODE) {
-				row+= '<td>Teléfono</td>'; 
-				row+= '<td>'+ element.text + '</td>';
-				idFreeCode = element.text;
-			} 
-			if(user.answers.indexOf(element) == DESCRIPTION) { 				
-				row+= '<td id="'+ idFreeCode +'">Points</td>';
-				row+= '<td>'+ element.text + '</td>';
-				row+= '<td>Fecha registro</td>'; 
-				row+= '<td>Promo id</td>'; 
-				row+= '<td>Acciones</td></tr>'; 
-			} 					
-		}); 
-		rowsCandidates.push(row);		
-    });
-	
-	$('#tbody').append(rowsCandidates);
-	return rowsCandidates;
-}
-
-showCandidatures();
-
-async function printPoints(){
-	await getFreeCodeIds();
-	var i = 0;
-
-	/* console.log('Array de IDs recien solicitados: ' + arrayFreeCodeIds);
-	console.log('Array 0 de IDs recien solicitados: ' + arrayFreeCodeIds[0]);
-	console.log('Array 1 de IDs recien solicitados: ' + arrayFreeCodeIds[1]); */
-	
-	arrayFreeCodeIds.forEach(element => {
-		async function printHtml(){
-			
-			try{
-				await getArrayPoints(element);			
-				//console.log('Puntos de ' + userArrayPoints[i].id + ':' + userArrayPoints[i].points);
-				document.getElementById(userArrayPoints[i].id).innerHTML=userArrayPoints[i].points;							
-				i++;
-			}catch(err){
-				console.error(err);
-				location.reload();
-			}			
-		};
-		printHtml();
-	});	
-}
-
-printPoints();
-
-</script>
-
