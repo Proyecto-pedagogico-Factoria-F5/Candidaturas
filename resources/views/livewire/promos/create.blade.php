4745ff128d90840525ab3801e49a13fe2baa8e8f
elegarmi <elegarmi@gmail.com>
2022-04-18 21:56:07 +0200

    Datapicker working at component

--- resources/views/livewire/promos/create.blade.php
+++ resources/views/livewire/promos/create.blade.php
@@ -9,7 +9,47 @@
                 </button>
             </div>
             <div class="modal-body">
-               <x-create-promo wire:model="start_date" id="start_date" />
+                <form wire:submit.prevent="save">
+                    <div class="form-group">
+                        <label for="school_id">Escuela<span class="required">*</span></label>
+                        <select wire:model="school_id" class="form-control form-select" name="school_id" required>
+                            <option value="0">Escoger escuela...</option>
+                            @foreach($schools as $school)
+                            <option value="{{$school->id}}">{{$school->name}}</option>
+                            @endforeach
+                        </select>@error('school_id') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Nombre<span class="required">*</span></label>
+                        <input wire:model="name" type="text" class="form-control" id="name" placeholder="Nombre" required>@error('name') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="ubication">Localidad<span class="required">*</span></label>
+                        <input wire:model="ubication" type="text" class="form-control" id="ubication" placeholder="Localidad" required>@error('ubication') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>   
+                    
+                    <x-datepicker wire:model="start_date" id="start_date" />
+
+                    <div class="form-group">
+                        <label for="duration">Duración (h)<span class="required">*</span></label>
+                        <input wire:model="duration" type="text" class="form-control" id="hours" placeholder="Duración (h)" required>@error('duration') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Imagen<span class="required">*</span></label>
+                        <input wire:model="image" type="file" class="form-control" id="image" name="image" placeholder="Imagen" required>@error('image') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="code">Código<span class="required">*</span></label>
+                        <input wire:model="code" type="text" class="form-control" id="code" placeholder="Código">@error('code') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="url">Url<span class="required">*</span></label>
+                        <input wire:model="url" type="text" class="form-control" id="url" placeholder="Url" required>@error('url') <span class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
+                </form>
+                <div>
+                    <span class="required">* Campos requeridos</span>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Cancelar</button>
